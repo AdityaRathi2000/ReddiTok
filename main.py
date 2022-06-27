891d6b96306d6f07a6e496f1d2859faea1448a78
Aditya Rathi <76499706+AdityaRathi2000@users.noreply.github.com>
2022-06-27 17:00:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,11 +9,4 @@ import time
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     aita_reddit_stories.main_func("AmITheAsshole", "month")
-    # driver = webdriver.Chrome('/Users/adityarathi/PycharmProjects/RedditTok/chromedriver')
-    # driver.get('https://www.tiktok.com/login')  # Manually login
-    # time.sleep(50)
-    # driver.get('https://www.tiktok.com/upload/?lang=en') # go on main page
-    # time.sleep(3)
-    #
-    # driver.quit()
 
